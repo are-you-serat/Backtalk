6d9889cf0412afda2e3893adf5c94b7892e8a763
kys0ff <idk683120@gmail.com>
2026-04-21 21:58:39 +0100

    build: Remove foojay-resolver-convention plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 @file:Suppress("UnstableApiUsage")
 
-
 pluginManagement {
     repositories {
         google {
@@ -13,9 +12,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
