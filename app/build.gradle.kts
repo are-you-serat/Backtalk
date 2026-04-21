89559ed7d64cb8da14fd68599ccca0c6689ed9cf
kys0ff <idk683120@gmail.com>
2026-04-21 11:50:28 +0100

    build: Update dependencies and clean up Gradle configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.devtools.ksp)
 }
@@ -44,14 +43,23 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+    sourceSets {
+        getByName("debug") {
+            kotlin.directories += "build/generated/ksp/debug/kotlin"
+        }
+        getByName("release") {
+            kotlin.directories += "build/generated/ksp/release/kotlin"
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
     }
 }
 
